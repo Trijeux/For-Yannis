c11df38e1c5c7d3868f6c529ba0631f73c302e66
DESKTOP-CFOLVV6\antho <anthonybarman2003@gmail.com>
2025-05-15 20:00:38 +0200

    Init For Yannis

---
+++ game/include/game.h
@@ -0,0 +1,9 @@
+#ifndef GAME_H
+#define GAME_H
+
+namespace Game
+{
+	void Loop();
+};
+
+#endif //GAME_H
---
+++ game/src/Game.cc
@@ -0,0 +1,44 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "resource_manager.h"
+#include "tilemap.h"
+
+namespace Game
+{
+	namespace
+	{
+		sf::RenderWindow window;
+		Tilemap     tilemap;
+
+		void CreateTilemap()
+		{
+			tilemap.Setup(sf::Vector2u(window.getSize().x / tilemap.SpritSize().x, window.getSize().y / tilemap.SpritSize().y));
+
+			tilemap.SetTiles();
+		}
+
+		void Setup()
+		{
+			window.create(sf::VideoMode({1600, 800}), "SFML Window");
+			ResourceManager::Setup();
+			CreateTilemap();		}
+	} // namespace
+
+	void Loop()
+	{
+		Setup();
+		while(window.isOpen())
+		{
+			while(const std::optional event = window.pollEvent())
+			{
+				if(event->is<sf::Event::Closed>())
+				{
+					window.close();
+				}
+			}
+
+			window.clear();
+			window.draw(tilemap);
+			window.display();
+		}
+	}
+} // namespace game
